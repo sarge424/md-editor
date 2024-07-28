8d86bf3e18445ea5063494d5e85c9ca9cd0b72f8
Arjun Sivakumar <arjunsivakumar2@gmail.com>
2024-07-28 22:05:27 +0530

    row creation from chunk data

--- editor/editor.go
+++ editor/editor.go
@@ -1,13 +1,20 @@
 package editor
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
+type row struct {
+	index  int
+	length int
+}
+
 type Editor struct {
 	text content
+	rows []row
 }
 
 func New(chunkSize int) Editor {
@@ -24,7 +31,6 @@ func (e *Editor) LoadFile(file string) {
 
 	data := strings.ReplaceAll(string(dat), "\t", "    ")
 
-	// load the file as chunks
 	for len(data) >= e.text.chunkSize {
 		e.text.append(data[:e.text.chunkSize])
 		data = data[e.text.chunkSize:]
@@ -34,8 +40,41 @@ func (e *Editor) LoadFile(file string) {
 	if len(data) > 0 {
 		e.text.append(data)
 	}
+
+	// create rows
+	e.rows = nil
+	chunkIndex := 0
+	last := -1
+	for i, ch := range e.text.chunks {
+		nextLn := strings.Index(ch, "\n")
+		for ; nextLn >= 0; nextLn = strings.Index(ch, "\n") {
+			r := row{
+				index:  last + 1,
+				length: chunkIndex + nextLn - last - 1,
+			}
+			last = chunkIndex + nextLn
+			ch = strings.Replace(ch, "\n", "Q", 1)
+
+			e.rows = append(e.rows, r)
+		}
+
+		//last row
+		if i == len(e.text.chunks)-1 {
+			r := row{
+				index:  last + 1,
+				length: chunkIndex + len(ch) - last - 1,
+			}
+			e.rows = append(e.rows, r)
+		}
+
+		chunkIndex += len(ch)
+	}
 }
 
 func (e Editor) String() string {
 	return e.text.String()
+}
+
+func (e Editor) Rows() {
+	fmt.Printf("%#v\n", e.rows)
 }
--- test.go
+++ test.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sarge424/notes/editor"
 )
 
 var (
-	path = "C:/Users/arjun/Desktop/vault/crypto.md"
+	path = "C:/Users/arjun/Desktop/vault/test.md"
 )
 
 func main() {
 	ed := editor.New(100)
 	ed.LoadFile(path)
 
-	fmt.Println(ed)
+	ed.Rows()
 }
